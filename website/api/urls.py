0e64285beca824b2b5924870ceac7ade2f5d414e
Kjeon7 <kenny.jeon25@gmail.com>
2022-03-26 21:52:35 -0600

    Small changes

--- website/api/urls.py
+++ website/api/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('getUsers/', views.UserList.getUsers),
     path('addUser/', views.UserList.addUser),
     path('getUser/<int:id>', views.UserDetail.getDetails) #to access certain user do site/getUser/id
+    
 
 ]
