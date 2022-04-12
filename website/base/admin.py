3515e118f53ccea88a4f3db475bd7ba22b92aba4
Kjeon7 <kenny.jeon25@gmail.com>
2022-04-12 16:26:32 -0600

    Can now get model name from parts when select is pressed

--- website/base/admin.py
+++ website/base/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import CPU, GPU, Memory, MotherBoard, Case, AirCooling, LiquidCooling, PowerSupply, Storage, User
+from .models import CPU, GPU, Memory, MotherBoard, Case, AirCooling, LiquidCooling, PowerSupply, Storage, User, Build
 
 admin.site.register(CPU)
 admin.site.register(GPU)
@@ -12,4 +12,5 @@ admin.site.register(AirCooling)
 admin.site.register(LiquidCooling)
 admin.site.register(PowerSupply)
 admin.site.register(Storage)
+admin.site.register(Build)
 admin.site.register(User)
--- website/base/views.py
+++ website/base/views.py
@@ -1,75 +1,84 @@
 from distutils.command.build import build
 from urllib.parse import uses_relative
-from django.http import HttpResponse
+from django.http import HttpResponse, QueryDict
 from django.shortcuts import redirect, render
 from .forms import RecommendForm, RecoveryForm, SignupForm, LoginForm
 from django.contrib.auth.models import User
 from .models import CPU, LiquidCooling, AirCooling, MotherBoard, PowerSupply, User, GPU, Memory, Storage, Case, Build
 from django.conf import settings
 from django.core.mail import send_mail
+from django.contrib import messages
+
 
 def showHome(request):
 
-    return render (request, 'home.html')
+    return render(request, 'home.html')
 
 
 def get_cpu(request):
     latest_cpu_list = CPU.objects.all()
     context = {'latest_cpu_list': latest_cpu_list}
+    model = request.GET
+    # cpu = Build.objects.get(build_id = 0)
+    # cpu.build_cpu = model.get('model')
+    # cpu.save()
     return render(request, 'cpu.html', context)
+
 
 def get_gpu(request):
     gpu_list = GPU.objects.all()
-    
+
     context = {'gpu_list': gpu_list}
     return render(request, 'gpu.html', context)
+
 
 def get_motherboard(request):
     motherboard_list = MotherBoard.objects.all()
     context = {'motherboard_list': motherboard_list}
     return render(request, 'motherboard.html', context)
 
+
 def get_psu(request):
     psu_list = PowerSupply.objects.all()
     context = {'psu_list': psu_list}
     return render(request, 'psu.html', context)
 
+
 def get_ram(request):
     ram_list = Memory.objects.all()
     context = {'ram_list': ram_list}
-
-   
-
     return render(request, 'memory.html', context)
+
 
 def get_storage(request):
     storage_list = Storage.objects.all()
     context = {'storage_list': storage_list}
     return render(request, 'storage.html', context)
 
+
 def get_case(request):
     case_list = Case.objects.all()
     context = {'case_list': case_list}
     return render(request, 'case.html', context)
+
 
 def get_airCool(request):
     ac_list = AirCooling.objects.all()
     context = {'ac_list': ac_list}
     return render(request, 'airCooling.html', context)
 
+
 def get_liquidCool(request):
     lc_list = LiquidCooling.objects.all()
     context = {'lc_list': lc_list}
 
-    #test = LiquidCooling(model_name='is this here?', price=1000, radiator_size='a', location='b')
-    #test.save()
-
     return render(request, 'liquidCooling.html', context)
+
 
 def showSignUp(request):
     submitbutton = request.POST.get("submit")
 
-    username =''
+    username = ''
     email = ''
     password = ''
 
@@ -79,19 +88,29 @@ def showSignUp(request):
             username = form.cleaned_data.get("username")
             email = form.cleaned_data.get("email")
             password = form.cleaned_data.get("password")
-            newUser = User.objects.create_user()
-            newUser = User(username=username, email=email, password=password)
-            newUser.save()
+            user = User.objects.create_user(username, email,password)
+            # newUser = User(username=username, email=email,
+            #                password=password, build_id=0)
+            # build = Build(build_id=0, name='', total_price=0, build_cpu='', build_gpu='', build_motherboard='', build_psu='',
+            #               build_ram='',
+            #               build_storage1='',
+            #               build_storage2='',
+            #               build_case='',
+            #               build_liquidCooling='',
+            #               build_airCooling=''
+            #               )
+            # newUser.save()
+            # build.save()
             return redirect('../signin')
     else:
         form = SignupForm()
-    context = {'form':form, 'submitbutton': submitbutton}
+    context = {'form': form, 'submitbutton': submitbutton}
     return render(request, 'newaccount.html', context)
 
 
 def showSignIn(request):
     submitbutton = request.POST.get("submit")
-    username =''
+    username = ''
     password = ''
 
     if request.method == 'POST':
@@ -99,17 +118,17 @@ def showSignIn(request):
         if form.is_valid():
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
-            usercheck = User.objects.filter(username = username, password = password)
+            usercheck = User.objects.get(username=username)
 
-        
     else:
         form = SignupForm()
-    context = {'form':form, 'submitbutton': submitbutton}
+    context = {'form': form, 'submitbutton': submitbutton}
     return render(request, 'signin.html', context)
 
 
 def showBuildPage(request):
-    return render (request, 'build.html')
+    return render(request, 'build.html')
+
 
 def showRecommendedPage(request):
     submitbutton = request.POST.get("submit")
@@ -121,23 +140,32 @@ def showRecommendedPage(request):
             choice = form.cleaned_data.get("choice")
     else:
         form = RecommendForm()
-    context = {'form':form, 'submitbutton':submitbutton}
-    return render (request, 'recommend.html', context)
+    context = {'form': form, 'submitbutton': submitbutton}
+    return render(request, 'recommend.html', context)
+
 
 def showRecoveryPage(request):
     submitbutton = request.POST.get("submit")
     email = ''
+    password = ''
     if request.method == 'POST':
         form = RecoveryForm(request.POST)
         if form.is_valid():
-            email = form.cleaned_data.get("email")     
-            subject = 'Monkee PC Builder Password Recovery'
-            message = 'Hey there, You forgot your password, here it is!'
-            email_from = settings.EMAIL_HOST_USER
-            recipient_list = [email]
-            send_mail( subject = subject, message = message, from_email=email_from, recipient_list = recipient_list )
-            return redirect('../')
+            email = form.cleaned_data.get("email")
+            try:
+                user = User.objects.get(email=email)
+                password = user.password
+                subject = 'Monkee PC Builder Password Recovery'
+                message = 'Hey there, You forgot your password, here it is!\n'
+                message += 'Password: ' + password
+                email_from = settings.EMAIL_HOST_USER
+                recipient_list = [email]
+                send_mail(subject=subject, message=message,
+                          from_email=email_from, recipient_list=recipient_list)
+                return redirect('../')
+            except User.DoesNotExist:
+                messages.error(request, "This is email is not registered!")
     else:
         form = RecoveryForm()
-    context = {'form':form, 'submitbutton':submitbutton}
-    return render (request, 'recovery.html', context)
+    context = {'form': form, 'submitbutton': submitbutton}
+    return render(request, 'recovery.html', context)
